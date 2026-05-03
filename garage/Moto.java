5af021731159c6fb3b0bc60c1acf9c0d7ffe1c80
MaxiPineyrua <111310426+MaxiPineyrua@users.noreply.github.com>
2026-05-03 13:22:10 -0300

    Creación de la subclase Moto

---
+++ garage/Moto.java
@@ -0,0 +1,35 @@
+package garage;
+
+//Moto extendes (hereda) de Vehiculo
+public class Moto extends Vehiculo {
+  // Constructor (uso super para llamar a la clase padre)
+  public Moto(String patente, String marca, String modelo, int horasEstimadas) {
+    super(patente, marca, modelo, horasEstimadas);
+  }
+
+  // Metodo calcularCosto (moto $700 por hora)
+  @Override
+  public double calcularCosto() {
+    return horasEstimadas * 700;
+  }
+
+  // Metodo getEspacioOcupado (moto ocupa 1)
+  @Override
+  public int getEspacioOcupado() {
+    return 1;
+  }
+
+  // Metodo mostrarDatos (muestra los datos de la moto)
+  @Override
+  public void mostrarDatos() {
+    System.out.println("------------------");
+    System.out.println("------ MOTO ------");
+    System.out.println("------------------");
+    System.out.println("Patente: " + patente);
+    System.out.println("Marca: " + marca);
+    System.out.println("Modelo: " + modelo);
+    System.out.println("Horas estimadas: " + horasEstimadas);
+    System.out.println("Costo estimado: $" + calcularCosto());
+  }
+
+}
