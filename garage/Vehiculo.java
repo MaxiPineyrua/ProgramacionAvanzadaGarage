99ed3cb27306023b4c9fa707b84e5f82a46661eb
MaxiPineyrua <111310426+MaxiPineyrua@users.noreply.github.com>
2026-05-03 13:11:33 -0300

    Creación de la clase Vehículo

---
+++ garage/Vehiculo.java
@@ -0,0 +1,45 @@
+package garage;
+
+//Abstract porque no existe un vehículo genérico.
+public abstract class Vehiculo {
+  protected String patente; // protected para que las subclases accedan.
+  protected String marca;
+  protected String modelo;
+  protected int horasEstimadas;
+
+  // constructor
+  public Vehiculo(String patente, String marca, String modelo, int horasEstimadas) {
+    this.patente = patente;
+    this.marca = marca;
+    this.modelo = modelo;
+    this.horasEstimadas = horasEstimadas;
+  }
+
+  // Getters
+  public String getPatente() {
+    return patente;
+  }
+
+  public String getMarca() {
+    return marca;
+  }
+
+  public String getModelo() {
+    return modelo;
+  }
+
+  public int getHorasEstimadas() {
+    return horasEstimadas;
+  }
+
+  // Métodos abstractos para que las subclases implementen
+  // Cada vehículo calcula su costo a su manera.
+  public abstract double calcularCosto();
+
+  // Cada vehículo indica cuanto ocupa
+  public abstract int getEspacioOcupado();
+
+  // cada vehiculo muestra sus datos
+  public abstract void mostrarDatos();
+
+}
